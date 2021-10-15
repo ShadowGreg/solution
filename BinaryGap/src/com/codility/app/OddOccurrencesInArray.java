c5bc077d187ace426c35d58c6accc5dd7307df1b
Greg Osetrov <shadow-greg@yandex.ru>
2021-10-15 14:56:32 +0300

    complit OddOccurrencesInArray

--- BinaryGap/src/com/codility/app/OddOccurrencesInArray.java
+++ BinaryGap/src/com/codility/app/OddOccurrencesInArray.java
@@ -3,12 +3,23 @@ package com.codility.app;
 public class OddOccurrencesInArray {
     public int solution(int[] A) {
 	int nonPaired = 0;
-//	for (int j = A.length; j > 1; j--) {
-	
-	for(int i=0; i<A.length-1; i++) {
-	    
+	int numberPasses = A.length-1;
+
+	for (int j = 0; j < numberPasses; j++) {
+
+	    for (int i = 1; i < numberPasses; i++) {
+		if (A[i] > 0) {
+		    if (A[j] == A[i]) {
+			A[j] = -10;
+			A[i] = -10;
+		    }
+		    if (A[i] > 0) {
+			nonPaired = A[i];
+		    }
+		}
+	    }
 	}
-	
+
 	return nonPaired;
     }
 }
--- BinaryGap/src/com/codility/app/Solution.java
+++ BinaryGap/src/com/codility/app/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 
     public static void main(String[] args) {
 	OddOccurrencesInArray intArray = new OddOccurrencesInArray();
-	int [] A= {9,3,9,3,9,7,9};
+	int [] A= {9, 3, 9, 3, 9, 7, 9};
 	
 	System.out.println(intArray.solution(A));
 
